6daa94d2b3cecc1ba897bf1c69fa95846add0897
rajmohan-14 <rajmohan14k@gmail.com>
2026-04-14 14:01:04 +0530

    : Real-time WebSocket monitoring with trust score updates

--- exammonitor/asgi.py
+++ exammonitor/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exammonitor.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from monitoring.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exammonitor.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
